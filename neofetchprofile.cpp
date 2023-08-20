f986d035dbb6415ff0f2a2a8be84513212a695c6
melone <mel@ne.nil>
2023-08-20 23:55:01 +0200

    File-Buffer

--- neofetchprofile.cpp
+++ neofetchprofile.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-int main(){
-    cout << "hello";
+
+int main () {
+    ifstream file;
+     file.open("/home/melone/Dokumente/.bashtest");
+    
+    if (file.fail())
+    {
+        cout << "file failed";
+        return 1;
+    }
+    
+    stringstream buffer;
+
+    buffer << file.rdbuf();
+
+    string file_content;
+
+    file_content = buffer.str();
+
+    file.close();
+
+    cout << file_content;
+
 }
